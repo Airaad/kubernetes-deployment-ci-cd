67328ed2a147c2c5872dcc52135e925944343e3e
airaad <sheikhairaad@gmail.com>
2025-09-01 14:58:26 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World from Kubernetes Cluster !!!!!!!!!"));
+
+app.get("/cpu", (req, res) => {
+  for (let i = 0; i < 100000000; i++) {
+    Math.random();
+  }
+  res.send("Running CPU intensive task");
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
